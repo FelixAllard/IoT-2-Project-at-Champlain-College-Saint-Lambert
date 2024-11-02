5061c8d36893204c0e272dc5624b27a22a7ee580
JustinMorissette <justin.dobbylechat@gmail.com>
2024-11-02 01:32:39 -0400

    Added event stream placeholders for it to be easier when connecting the actual sensors. Added fake changing data. Revamped front-end to show in a  body-structure the waist, knees and feet.

--- RaspberryPiApi/App.py
+++ RaspberryPiApi/App.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask import Response
+import json
+import time
+import random
 
 from Model.Sensor import Sensor
 
@@ -13,9 +17,11 @@ raspberryPiCredentials = {
 }
 
 sensors = [
-    Sensor(1, "Temperature Sensor", 10.0, 20.0, 5.0, 1.2, 85.0),
-    Sensor(2, "Humidity Sensor", 15.5, 25.0, 7.8, 0.5, 60.0),
-    Sensor(3, "Pressure Sensor", 18.0, 30.0, 12.0, 2.1, 50.0)
+    Sensor(1, "Left Foot", 10.0, 20.0, 5.0, 1.2, 85.0),
+    Sensor(2, "Right Foot", 15.5, 25.0, 7.8, 0.5, 60.0),
+    Sensor(3, "Left Knee", 18.0, 30.0, 12.0, 22.1, 50.0),
+    Sensor(4, "Right Knee", 11.0, 37.0, 32.0, 6.1, 70.0),
+    Sensor(5, "Waist", 39.0, 34.0, 32.0, 3.1, 100.0)
 ]
 
 @app.route('/ping', methods=['GET'])
@@ -23,9 +29,28 @@ def ping():
     current_time = datetime.now().isoformat()
     return jsonify({'message': 'pong', 'datetime': current_time}), 200
 
-@app.route('/sensors', methods=['GET'])
-def get_sensors():
-    return jsonify([sensor_to_dict(sensor) for sensor in sensors])
+@app.route('/sensor-stream', methods=['GET'])
+def stream_sensors():
+    def event_stream():
+        while True:
+            for sensor in sensors:
+                #Fake data, will have to be replaced when connecting to actual sensor data
+                velocity_change = random.uniform(-1.0, 1.0)
+                sensor.velocity += velocity_change
+
+                sensor.velocity = max(0, min(sensor.velocity, 30.0))
+
+                sensor.x += sensor.velocity * 0.1
+                sensor.y += sensor.velocity * 0.1
+                sensor.z += sensor.velocity * 0.1
+
+                sensor.battery_percentage = max(sensor.battery_percentage - 0.01, 0)
+
+            data = [sensor_to_dict(sensor) for sensor in sensors]
+            yield f"data: {json.dumps(data)}\n\n"
+            time.sleep(0.1)
+
+    return Response(event_stream(), mimetype="text/event-stream")
 
 @app.route('/id', methods=['GET'])
 def get_raspberry_pi_id():
